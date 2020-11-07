64ddb03fbb04fe34b97702eecd3a661a1b15bbc6
elghaied <elghaied.eslam@gmail.com>
2020-11-07 05:21:34 +0100

    commit before making some Modifications

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,66 +5,23 @@ export class Card extends Component {
     constructor(props) {
         super(props);
         this.state ={
-            showCard: false,
-            card1: "",
-            card2: "",
-            match: false,
-      
+          name: this.props.name,
+          showCard: this.props.showCard
         };
-        this.setVisible = this.setVisible.bind(this);
        
       }
-      setVisible = (event) => {
-        if(!this.state.match){
-          if(this.state.card1 !== "" && this.state.card2 === ""){
-            this.setState({
-              showCard: !this.state.showCard,
-              card2: this.props.name,
-            })
-            console.log("after 2 card")
 
-            if (this.state.card1 == this.state.card2){
-              console.log("1point")
-              this.setState({
-                match: true,
-              })
-            }if(this.state.card1 !== this.state.card2) {
-              console.log("hide false")
-              this.setState({
-                
-                showCard: false,
-                card1: "",
-                card2: ""
-              })
-            }
-          }
-          if(this.state.card1 === "" && this.state.card2 === ""){
 
       
-              this.setState({
-                  showCard: !this.state.showCard,
-                  card1: this.props.name,
-                
-
-                });    
-          }
-            console.log("here you go");
-            console.log(this.props.name);
-        }
-    }
     render() {
-        const {showCard} = this.state;
       const {name,image} = this.props;
-            
-       
-            
+      const {showCard } = this.state;
                     return (
                 
 
                         <div
                         className = "Card"
-                        onClick = {this.setVisible}
-                        value = {name}
+                        onClick = { (e) => this.updateCard(e)}
                         >
                         {showCard ?
                         <img 
@@ -75,6 +32,14 @@ export class Card extends Component {
                     )
                 
 
+                    }
+
+                    updateCard (e){
+
+                      this.setState({
+                        name: this.props.name
+                      })
+                      this.props.setVisible(e);
                     }
                 }
         
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -7,11 +7,70 @@ export class GameBoard extends Component {
     constructor(props) {
         super(props)
         this.state ={
+            showCard: false,
+            match: false,
+            card1:"",
+            card2:"",
        
         }
+        this.setVisible = this.setVisible.bind(this);
+
       }
+      setVisible = (e) => {
+
+        this.setState({
+            showCard: !this.state.showCard,
+            
+           
+        });
+        console.log("this is name on GameBoard.js >>",this.props.name);
+        console.log(e);
+        
+        // if(!this.state.match){
+        //   if(this.state.card1 !== "" && this.state.card2 === ""){
+        //     console.log( "card1 full and card2 empty")
+
+        //     this.setState({
+        //       showCard: !this.state.showCard,
+        //       card2: e.props.value,
+        //     })
+        //     console.log("after 2 card")
+
+        //     if (this.state.card1 === this.props.name){
+        //       console.log("1point")
+        //       this.setState({
+        //         match: true,
+        //       });
+
+        //     }
+        //     if(this.state.card1 !== this.state.card2) {
+        //       console.log("hide false")
+        //       this.setState({
+                
+        //         showCard: false,
+        //         card1: "",
+        //         card2: ""
+        //       })
+        //     }
+        //   }
+        //   if(this.state.card1 === "" && this.state.card2 === ""){
+
       
+        //       this.setState({
+        //           showCard: !this.state.showCard,
+        //           card1: e.props.value,
+                
+
+        //         });    
+        //         console.log( "card1 and card2 empty")
+        //   }
+        //     console.log("here you go");
+        //     console.log(this.props.name);
+        // }
+    }
     render() {
+                const {showCard} = this.state;
+
         return (
             <div className="GameBoard">
          
@@ -19,7 +78,7 @@ export class GameBoard extends Component {
             <Music/>
 
             <div className="Board-Bg">
-                {CardsData.sort(()=> Math.random() -0.5).map((card, index) => {
+                { CardsData.sort(()=> Math.random() -0.5).map((card, index) => {
                   
 
                     return(
@@ -29,6 +88,8 @@ export class GameBoard extends Component {
                         name = {card.name}
                         image = {card.image}
                         quantity = {card.quantity}
+                        showCard = {showCard}
+                        setVisible = {this.setVisible}
                         />
              )
                 })}
